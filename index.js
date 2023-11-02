187fa320de8ef4302e363fa8bfdbdeb9803d268e
MohmedAyoubJs <113200512+Mohmed-Ayoub-Js@users.noreply.github.com>
2023-11-03 00:48:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const port = 3001;
 app.use(express.json());
 const corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:5173',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 
@@ -23,23 +23,29 @@ if(!con){
     console.log('error creating connection');
 }
 app.post('/', (req, res) => {
-   const name = req.body.name;
-   const size = req.body.size;
-   const image = req.body.image;
-   const developer = req.body.developer;
-   const type = req.body.type;
-   const des = req.body.des;
-   const moreImage = req.body.moreImage;
-   const link = req.body.link;
-   const sql = 'INSERT INTO software  (name, size, image, developer, type, des, moreImage , link) VALUES (?, ?, ?, ?, ?, ?, ? , ?)';
-   con.query(sql, [name, size, image, developer, type, des, moreImage , link], (err, results) => {
+  const name = req.body.name;
+  const size = req.body.size;
+  const image = req.body.image;
+  const developer = req.body.developer;
+  const type = req.body.type;
+  const des = req.body.des;
+  const moreImage = req.body.moreImage;
+  const link = req.body.link;
+  const view = req.body.view;
+  const updatedView = view + 1;
+  const sql = 'INSERT INTO software  (name, size, image, developer, type, des, moreImage, link, view) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  con.query(sql, [name, size, image, developer, type, des, moreImage, link, updatedView], (err, results) => {
     if (err) {
       console.error('خطأ في إدراج البيانات:', err);
+      res.status(500).json({ error: 'خطأ في إدراج البيانات' });
     } else {
       console.log('تم إدراج البيانات بنجاح');
+      // قم بإرجاع نجاح العملية إلى العميل
+      res.json({ success: true });
     }
   });
 });
+
 
 
 app.get('/software', (req, res) => {
